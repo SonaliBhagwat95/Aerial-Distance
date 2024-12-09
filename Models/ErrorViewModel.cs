8c52ad76639c807c85dfe5023d3dc4d0c9e82578
Admin <Admin@ARY004>
2024-12-09 09:59:03 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aerial_Distance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
