b18b968b5ac4cbf5f6c9387eb0baa3325de815cb
Sonali Bhagwat <sonali@aryamantechnologies.com>
2024-12-09 18:30:59 +0530

    Application Details New

--- Controllers/ApplicationDetailsNew.cs
+++ Controllers/ApplicationDetailsNew.cs
@@ -1,16 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AerialDistance.Model;
+using AerialDistance.Repositories.Contracts;
+using AerialDistance.Repositories.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AerialDistance.Controllers
 {
-    public class ApplicationDetailsNewController : Controller
+    public class ApplicationDetailsNewController : BaseController
     {
+        private UnitOfWorks _uow;
+        public ApplicationDetailsNewController(IUnitOfWork uow, IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+            this._uow = uow as UnitOfWorks;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult ApplicationDetails()
+        public IActionResult ApplicationDetails(ApplicationDetailsNewModel ApplicationDetailsNewModel)
         {
-            return View();
+            try
+            {
+                if (ApplicationDetailsNewModel.ApplicationNo == null)
+                {
+                    return View();
+                }
+                ApplicationDetailsNewModel.MobileNo = 99;
+                ApplicationDetailsNewModel.MobileNo2 = 99;
+                ApplicationDetailsNewModel.CreatedBy = 99;
+                ApplicationDetailsNewModel.UploadFilePath = "sa";
+
+                ApplicationDetailsNewModel model = ApplicationDetailsNewModel;
+                int flag = _uow.ApplicationDetailsRepository.SaveApplicatonDetails(model);
+                if (flag == 0 || flag == -1)
+                    return RedirectToAction("Index", "ApplicationDetailsNewController");
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                return View();
+            }
         }
     }
 }
