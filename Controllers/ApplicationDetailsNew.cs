2d3963bc9df31fb65744bca5a6a0f3db6893dbd7
Sonali Bhagwat <sonali@aryamantechnologies.com>
2024-12-09 16:57:34 +0530

    Application Details New Save

--- Controllers/ApplicationDetailsNew.cs
+++ Controllers/ApplicationDetailsNew.cs
@@ -1,15 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AerialDistance.Model;
+using AerialDistance.Repositories.Contracts;
+using AerialDistance.Repositories.Repositories;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AerialDistance.Controllers
 {
-    public class ApplicationDetailsNewController : Controller
+    public class ApplicationDetailsNewController : BaseController
     {
+        private  UnitOfWorks? _uow;
+
+        public ApplicationDetailsNewController(IUnitOfWork uow, IHttpContextAccessor httpContextAccessor) : base(httpContextAccessor)
+        {
+            this._uow = uow as UnitOfWorks;
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult ApplicationDetails()
+        public IActionResult ApplicationDetails(ApplicationDetailsNewModel ApplicationDetailsNewModel)
         {
+            if (ApplicationDetailsNewModel == null)
+            {
+                return RedirectToAction("Index", "ApplicationDetailsNewController");
+            }
+
+            ApplicationDetailsNewModel model = ApplicationDetailsNewModel;
+            int flag = _uow.ApplicationDetailsRepository.SaveApplicatonDetails(model);
+            if(flag == 0 || flag == -1)
+                return RedirectToAction("Index", "ApplicationDetailsNewController");
+
             return View();
         }
     }
